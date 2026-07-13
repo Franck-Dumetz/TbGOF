8f48317a1405b14c213a01f6484c14a5704a7028
anushka-shome <112298919+anushka-shome@users.noreply.github.com>
2026-07-13 17:12:37 -0400

    Update count-reads.py

--- count-reads.py
+++ count-reads.py
@@ -29,6 +29,7 @@ from collections import defaultdict
 import os
 
 def read_gff(fn, bam):
+    file_counts[bam] = 0
     fields = []
     with open(fn) as f:
         for line in f:
@@ -37,13 +38,16 @@ def read_gff(fn, bam):
             fields = line.strip().split("\t")
             if len(fields) < 9:
                 continue
+            gene = fields[8].split("gene_id=")[1].split(";")[0]
             count_reads(bam, fields)
+            des[fields[8].split("gene_id=")[1].split(";")[0]] = fields[8].split("Description=")[1].split(";")[0]
 
 def count_reads(bam, fields):
     chrom = fields[0]
     start = int(fields[3])
     end = int(fields[4])
-    name = fields[8].split(":")[0]
+    #name = fields[8].split(":")[0]
+    name = fields[8].split("gene_id=")[1].split(";")[0]
     cmd = ["samtools", "view", f"bam/{bam}", f"{chrom}:{start}-{end}"]
     result = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True, check=True)
     output = result.stdout.strip()
@@ -52,10 +56,13 @@ def count_reads(bam, fields):
     else:
         lines = result.stdout.strip().split("\n")
         counts[bam][name] = len(lines)
+        file_counts[bam] += len(lines)
     #print(f"{bam} {counts[bam][name]}")
     return counts
 
 counts = defaultdict(dict)
+des = defaultdict(dict)
+file_counts = defaultdict(dict)
 
 #bam_files = ["SRR10846669.bam", "SRR10846670.bam", "SRR10846671.bam", "SRR10846672.bam", "SRR10846673.bam", "SRR10846674.bam", "SRR10846675.bam", "SRR10846676.bam", "SRR10846677.bam", "SRR10846678.bam", "SRR10846679.bam", "SRR10846680.bam"] # change the name of the bam files here
 bam_files = [f for f in os.listdir("bam") if f.endswith(".bam") and os.path.isfile(os.path.join("bam", f))]
@@ -68,9 +75,6 @@ row_headers = sorted({row for col in counts.values() for row in col})
 col_headers = sorted(counts.keys())
 with open('counts.csv', 'w', newline='') as file: # change the output csv file name here
     writer = csv.writer(file)
-    writer.writerow(["Sample"] + [col.split('.')[0] for col in col_headers])
+    writer.writerow(["Gene"] + ["Description"] + [col.split('.')[0] for col in col_headers])
     for row in row_headers:
-        writer.writerow([row] + [counts[col].get(row, 0) for col in col_headers])
-
-
-
+        writer.writerow([row] + [des[row]] + [counts[col].get(row, 0) for col in col_headers])
