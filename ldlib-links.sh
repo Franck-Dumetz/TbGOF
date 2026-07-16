130cf75e38f4f71ab2ce40adf71e2c1ef615ede2
anushka-shome <112298919+anushka-shome@users.noreply.github.com>
2026-07-16 09:59:06 -0400

    Delete ldlib-links.sh

--- ldlib-links.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p "$CONDA_PREFIX/etc/conda/activate.d"
-mkdir -p "$CONDA_PREFIX/etc/conda/deactivate.d"
-
-cp change-ldpath/activate/linux_ld_library_path.sh "$CONDA_PREFIX/etc/conda/activate.d/"
-cp change-ldpath/deactivate/linux_ld_library_path.sh "$CONDA_PREFIX/etc/conda/deactivate.d/"
