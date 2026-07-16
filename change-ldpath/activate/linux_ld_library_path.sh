47733cec0956498de815ccf68b958d3096b6a937
anushka-shome <112298919+anushka-shome@users.noreply.github.com>
2026-07-16 09:58:52 -0400

    Delete change-ldpath directory

--- change-ldpath/activate/linux_ld_library_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ "$(uname)" == "Linux" ]]; then
-  export OLD_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
-  export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-fi
--- change-ldpath/deactivate/linux_ld_library_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ "$(uname)" == "Linux" ]]; then
-  export LD_LIBRARY_PATH="$OLD_LD_LIBRARY_PATH"
-  unset OLD_LD_LIBRARY_PATH
-fi
